886d3bf116ae522376f444a59978edb49f4c869d
Mike Peterson <mike@lemonshell.com>
2025-03-23 16:44:57 -0700

    small fixes

--- src/app/_components/GoogleAuthButton.tsx
+++ src/app/_components/GoogleAuthButton.tsx
@@ -1,15 +1,22 @@
-"use client";
-import { useSession } from "next-auth/react";
 import Link from "next/link";
+import { auth } from "~/server/auth";
 
-const GoogleAuthButton: React.FC = () => {
-  const { data: session, status } = useSession();
+const GoogleAuthButton: React.FC = async () => {
+  const session = await auth();
 
   if (session?.user.email) {
-    return <Link href="/auth/signout/" className="font-semibold text-sm">sign out</Link>;
+    return (
+      <Link href="/auth/signout/" className="text-sm font-semibold">
+        sign out
+      </Link>
+    );
   }
 
-  return <Link href="/auth/signin/" className="font-semibold text-sm">sign in</Link>;
+  return (
+    <Link href="/auth/signin/" className="text-sm font-semibold">
+      sign in
+    </Link>
+  );
 };
 
 export default GoogleAuthButton;
