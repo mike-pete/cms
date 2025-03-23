9af456290fce8f64ab5676728aeb0d61e7fc06b2
Mike Peterson <mike@lemonshell.com>
2025-03-23 16:47:26 -0700

    hide dashboard button when signed out

--- src/app/_components/NavBar.tsx
+++ src/app/_components/NavBar.tsx
@@ -1,17 +1,22 @@
 import Link from "next/link";
+import { auth } from "~/server/auth";
 import GoogleAuthButton from "./GoogleAuthButton";
 import Row from "./row";
 
-export default function NavBar() {
+export default async function NavBar() {
+  const session = await auth();
+
   return (
     <Row className="sticky top-0 z-50 w-full items-center border-b border-neutral-700 bg-neutral-950 p-4">
       <Row className="flex-1 items-center gap-4">
         <Link href="/" className="font-bold text-emerald-500">
           CMS
         </Link>
-        <Link href="/dashboard" className="text-sm font-semibold">
-          dashboard
-        </Link>
+        {session?.user && (
+          <Link href="/dashboard" className="text-sm font-semibold">
+            dashboard
+          </Link>
+        )}
       </Row>
       <GoogleAuthButton />
     </Row>
