957083b84341823ee89cded59cb2aa302fe5a1c3
Mike Peterson <mike@lemonshell.com>
2025-03-23 13:25:33 -0700

    tweak padding

--- src/app/(default)/dashboard/page.tsx
+++ src/app/(default)/dashboard/page.tsx
@@ -9,13 +9,13 @@ export default async function DashboardPage() {
   const hello = await api.post.hello({ text: "from tRPC" });
   return (
     <Row className="flex-grow">
-      <Col className="max-w-md flex-grow gap-4 border-r border-neutral-700 p-4">
+      <Col className="max-w-md flex-grow gap-4 border-r border-neutral-700 p-8">
         <CsvUpload />
         <Col className="rounded-md border border-neutral-700 p-4">
           <Col>No files being processed</Col>
         </Col>
       </Col>
-      <Col className="flex-grow p-4">
+      <Col className="flex-grow p-8">
         <HydrateClient>
           <div>
             <p className="text-2xl text-white">
