2aa32d852d0e23426101877f84b722d4f13aac2c
Mike Peterson <mike@lemonshell.com>
2025-04-07 12:49:06 -0700

    add loading screen

---
+++ src/app/(default)/(protected)/dashboard/loading.tsx
@@ -0,0 +1,23 @@
+import Col from "~/components/Col";
+import Row from "~/components/Row";
+
+export default function Loading() {
+  return (
+    <Row className="h-full flex-1 overflow-hidden">
+      <Col className="flex h-full max-w-sm flex-1 gap-4 border-r border-neutral-700 p-8">
+        <SkeletonBox />
+      </Col>
+      <Col className="flex h-full flex-1 gap-4 p-8">
+        <Col className="min-h-0 flex-1">
+          <SkeletonBox />
+        </Col>
+      </Col>
+    </Row>
+  );
+}
+
+function SkeletonBox() {
+  return (
+    <div className="h-full w-full animate-pulse rounded-md border border-neutral-700 bg-neutral-900" />
+  );
+}
